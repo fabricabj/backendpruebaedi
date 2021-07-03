4ea85e7fe24a6957f67a6726ed43075e20e6c2da
FrancoColavella <francocolavella1@gmail.com>
2021-07-03 19:29:00 -0300

    hola

--- app/controllers/peliculasController.php
+++ app/controllers/peliculasController.php
@@ -5,12 +5,7 @@ class peliculasController{
 public function RetornarPeliculas($request, $response, $args){
     //$listaDeParametros = $request->getParsedBody();
     // $hashDeContrasena = password_hash($listaDeParametros['nuevaContra'], PASSWORD_DEFAULT);
-
-    
-
-    Peliculas::obtenerPeliculas();
-    return $response;
-    $jsonpeliculas = Provincia::obtenerPeliculas();
+    $jsonpeliculas = Peliculas::obtenerPeliculas();
     $response->getBody()->Write(json_encode($jsonpeliculas));
     return $response ->withHeader('Content-Type', 'application/json');;
     //$response->getBody()->write(json_encode($usuario));
