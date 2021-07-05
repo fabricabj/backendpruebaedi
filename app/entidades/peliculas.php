ff83fa5aec3198cb4d9632895da922a68d360dd7
fabricabj <colavella23@gmail.com>
2021-07-05 02:01:33 -0300

    hola

--- app/entidades/peliculas.php
+++ app/entidades/peliculas.php
@@ -23,7 +23,7 @@
             $consulta->execute();
             $resultado_peliculas = $consulta->fetchAll();
             foreach($resultado_peliculas as $row){
-                    echo "<img style='width:300px;height:300px' src='".$row['imagen']."'>";
+                    echo "<img style='width:300px;height:300px' src='".$row['imagen']."'><p>".$row['titulo']."</p>";
         
             }
             
