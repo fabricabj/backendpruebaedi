869375e1537b22b0538884c001b6a2948a9d8f3a
fabricabj <colavella23@gmail.com>
2021-07-05 02:46:34 -0300

    hola

--- app/entidades/peliculas.php
+++ app/entidades/peliculas.php
@@ -23,10 +23,8 @@
             $consulta->execute();
             $resultado_peliculas = $consulta->fetchAll();
             foreach($resultado_peliculas as $row){
-                    echo "<img src='".$row['imagen']."'>";
-                    echo "<p>".$row['titulo']."</p>";
+                    echo "<img src='".$row['imagen']."'><p>".$row['titulo']."</p>";
      
-        
             }
             
             //return $consulta->fetchAll(PDO::FETCH_CLASS, 'peliculas');
