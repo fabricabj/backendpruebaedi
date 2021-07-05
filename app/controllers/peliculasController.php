8b4988964386ec4771c997423320007bb3fde849
fabricabj <colavella23@gmail.com>
2021-07-05 01:09:11 -0300

    hola

--- app/controllers/peliculasController.php
+++ app/controllers/peliculasController.php
@@ -5,19 +5,23 @@ class peliculasController{
 public function RetornarPeliculas($request, $response, $args){
     $listaDeParametros = $request->getParsedBody();
     // $hashDeContrasena = password_hash($listaDeParametros['nuevaContra'], PASSWORD_DEFAULT);
-   /* $jsonpeliculas = Peliculas::obtenerPeliculas();
+    $jsonpeliculas = Peliculas::obtenerPeliculas();
     $response->getBody()->Write(json_encode($jsonpeliculas));
-    return $response ->withHeader('Content-Type', 'application/json');;*/
+    return $response ->withHeader('Content-Type', 'application/json');;
     //$response->getBody()->write(json_encode($usuario));
 
     /*$arratProvincias = Peliculas::obtenerPeliculas();
     $response->getBody()->Write(json_encode($arratProvincias));
 
     return $response ->withHeader('Content-Type', 'application/json');;*/
-    Peliculas::obtenerPeliculas();
-        //$response->getBody()->write(json_encode($usuario));
 
-        return $response;
+
+
+
+    //Peliculas::obtenerPeliculas();
+       
+
+     //   return $response;
 }
 
 
