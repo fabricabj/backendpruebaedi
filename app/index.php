5e16c70bed1e6e494f9e6cc6204c172e49d9ddd3
FrancoColavella <francocolavella1@gmail.com>
2021-07-06 20:52:30 -0300

    hola

--- app/index.php
+++ app/index.php
@@ -67,6 +67,7 @@ $app->post('[/]', function (Request $request, Response $response, array $args) {
 $app->post('[/]', \usuarioController::class . ':CrearUsuario');
 $app->post('/login[/]', \usuarioController::class . ':retornarUsuario');
 $app->get('/peliculas[/]', \peliculasController::class . ':RetornarPeliculas');
+$app->post('/peliculas/alta[/]', \peliculasController::class . ':Alta');
 //$app->get('/peliculas[/]', \peliculasController::class . ':RetornarPeliculas');
 //$app->post('/peliculas[/]', \usuarioController::class . ':retornarUsuario');
 
