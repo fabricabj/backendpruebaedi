1f0a6614d5da70a77f4d1fd0a32b84a1d10b45fe
Fabricio Colavella <55010128+fabricabj@users.noreply.github.com>
2021-10-31 18:42:20 -0300

    Update AcessoDatos.php

--- app/acessoDatos/AcessoDatos.php
+++ app/acessoDatos/AcessoDatos.php
@@ -9,16 +9,17 @@ class AccesoDatos
         try {
             //
 
+            $this->objetoPDO = new PDO('mysql:host=remotemysql.com:3306;dbname=gD97u3NHFm;charset=utf8', 'gD97u3NHFm', 'h4RjoZTwHJ', array(PDO::ATTR_EMULATE_PREPARES => false, PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
             //$this->objetoPDO = new PDO('mysql:host='.getenv('ServidorMySQL').';dbname='.getenv('Database').';charset=utf8', getenv("Usuario"), getenv('Pass'), array(PDO::ATTR_EMULATE_PREPARES => false, PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
-            $this->objetoPDO = new PDO(
-                'mysql:host=remotemysql.com:3306;dbname=gD97u3NHFm;charset=utf8',
-                'gD97u3NHFm',
-                'h4RjoZTwHJ',
-                [
-                    PDO::ATTR_EMULATE_PREPARES => false,
-                    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-                ]
-            );
+            //$this->objetoPDO = new PDO(
+              //  'mysql:host=remotemysql.com:3306;dbname=gD97u3NHFm;charset=utf8',
+                //'gD97u3NHFm',
+                //'h4RjoZTwHJ',
+                //[
+                  //  PDO::ATTR_EMULATE_PREPARES => false,
+                    //PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+                //]
+            //);
             $this->objetoPDO->exec('SET CHARACTER SET utf8');
         } catch (PDOException $e) {
             print 'Error: ' . $e->getMessage();
